8e92b8dc68ef6b498301f6bcaf97add491518d4c
Luke Schipper <schipperlm@gmail.com>
2020-01-29 20:15:42 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <header className="app-header">
+        If you see this, the app is serving correctly.
+      </header>
+    </div>
+  );
+}
+
+export default App;
